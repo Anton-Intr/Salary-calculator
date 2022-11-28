6f48c9b7a6268ebcdb2674ceb7aa46f7004b8855
Anton-Intr <anton@interactivated.me>
2022-11-28 12:38:15 +0200

    Making certain fields required

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ app = FastAPI()
 class Params:
     def __init__(
         self,
-        account_key: str = Query(None, description='Jira key, you can get it by visiting your Jira profile and copy-pasting key from URL, like 5a4ce9e3ebeb8229f4b2d45a'),
-        tempo_token:str = Query(None, description='API token for Tempo integration. See docs for getting it: https://apidocs.tempo.io/#section/Authentication#using-the-rest-api-as-an-individual-user'),
-        users_rate:float = Query(None, description='Hourly rate'),
-        date_start: str = Query(None, description='Start date for salary calculation. Format: YYYY-MM-DD.'),
+        account_key: str = Query(description='Jira key, you can get it by visiting your Jira profile and copy-pasting key from URL, like 5a4ce9e3ebeb8229f4b2d45a'),
+        tempo_token:str = Query(description='API token for Tempo integration. See docs for getting it: https://apidocs.tempo.io/#section/Authentication#using-the-rest-api-as-an-individual-user'),
+        users_rate:float = Query(description='Hourly rate'),
+        date_start: str = Query(description='Start date for salary calculation. Format: YYYY-MM-DD.'),
         date_end: str = Query('now',description='End date for salary calculation. Leave now - to calculate till today (including). Format: YYYY-MM-DD.'),
         rate_multiplier: float = Query(1.5, description='User\'s rate multiplier. Normal is 1. Default is 1.5 now cause of paid by hour.'),
         education_rate_multiplier: float = Query(1, description='User\'s education rate multiplier. Normal is 1.'),
